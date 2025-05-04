9a95e581721244f2d5dba942d477a566a1c9c0a3
claudia-lulu <claudiabrunet98@gmail.com>
2025-05-04 19:25:04 +0200

    ajout variable de session id

--- connexion.php
+++ connexion.php
@@ -57,11 +57,12 @@ else
             {
                 try
                 {
-                    $stmt2 = $pdo->prepare("SELECT statut FROM utilisateur WHERE pseudo = :pseudo");
+                    $stmt2 = $pdo->prepare("SELECT id, statut FROM utilisateur WHERE pseudo = :pseudo");
                     $stmt2->bindParam(':pseudo', $pseudo);
                     $stmt2->execute();
                     $_SESSION['pseudo'] = $pseudo;
-                    $_SESSION['statut'] = $stmt2->fetch()[0];
+                    $_SESSION['id'] = $stmt2->fetch()[0];
+                    $_SESSION['statut'] = $stmt2->fetch()[1];
 
                     $stmt2->closeCursor();
                 }
