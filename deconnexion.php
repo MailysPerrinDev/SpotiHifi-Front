ec31bac1f384b40f7a65ce213ba2295b42de6253
claudia-lulu <claudiabrunet98@gmail.com>
2025-05-07 21:56:43 +0200

    Update deconnexion.php

--- deconnexion.php
+++ deconnexion.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+if (file_exists("img/".$_SESSION['pseudo'].".jpg")) //si l'utilisateur a une photo personnalisée
+{
+	unlink("img/".$_SESSION['pseudo'].".jpg");
+}
+
 session_unset();
 session_destroy();
 
